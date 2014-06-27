c095bc5ecb13ef323052bfc9de3ce2cbfa7b8577
lmcMedia <chris@liquidmindcreative.com>
2014-06-27 16:44:09 +0700

    update controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,99 +6,82 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-#---
-# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   # GET /products
-  # GET /products.xml
+  # GET /products.json
   def index
     @products = Product.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @products }
-    end
   end
 
   # GET /products/1
-  # GET /products/1.xml
+  # GET /products/1.json
   def show
-    @product = Product.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @product }
-    end
   end
 
   # GET /products/new
-  # GET /products/new.xml
   def new
     @product = Product.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @product }
-    end
   end
 
   # GET /products/1/edit
   def edit
-    @product = Product.find(params[:id])
   end
 
   # POST /products
-  # POST /products.xml
+  # POST /products.json
   def create
-    @product = Product.new(params[:product])
+    @product = Product.new(product_params)
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to(@product,
-          :notice => 'Product was successfully created.') }
-        format.xml  { render :xml => @product, :status => :created,
-          :location => @product }
+        format.html { redirect_to @product,
+          notice: 'Product was successfully created.' }
+        format.json { render action: 'show', status: :created,
+          location: @product }
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @product.errors,
-          :status => :unprocessable_entity }
+        format.html { render action: 'new' }
+        format.json { render json: @product.errors,
+          status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /products/1
-  # PUT /products/1.xml
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
   def update
-    @product = Product.find(params[:id])
-
     respond_to do |format|
-      if @product.update_attributes(params[:product])
-        format.html { redirect_to(@product,
-          :notice => 'Product was successfully updated.') }
-        format.xml  { head :ok }
+      if @product.update(product_params)
+        format.html { redirect_to @product,
+          notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @product.errors,
-          :status => :unprocessable_entity }
+        format.html { render action: 'edit' }
+        format.json { render json: @product.errors,
+          status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /products/1
-  # DELETE /products/1.xml
+  # DELETE /products/1.json
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
-
     respond_to do |format|
-      format.html { redirect_to(products_url) }
-      format.xml  { head :ok }
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
     end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white
+    # list through.
+    def product_params
+      params.require(:product).permit(:title, :description, :image_url, :price)
+    end
 end
